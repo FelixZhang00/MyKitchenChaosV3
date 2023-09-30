46f7dae2c1863f203c06818a0fad3bface83ee2f
felix <l836828946@gmail.com>
2023-09-30 08:53:21 +0800

    player interact

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,19 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GameInput : MonoBehaviour
 {
+    public event EventHandler OnInteractHandler;
     private PlayerInputActions playerInputActions;
 
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
+
+        playerInputActions.Player.Interact.performed += Interact_performed;
+    }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractHandler?.Invoke(this,EventArgs.Empty);
     }
 
     public Vector2 GetPlayerMoveInput()
     {
+        if (playerInputActions == null) { 
+            return Vector2.zero;
+        }
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
         inputVector = inputVector.normalized;
         return inputVector;
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,68 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     
-    [SerializeField] private GameInput input;
+    [SerializeField] private GameInput gameInput;
     [SerializeField] private float moveSpeed = 7.0f;
-    private bool isWalking = false;
+    [SerializeField] private LayerMask counterLayerMask;
 
+
+    private bool isWalking = false;
+    private Vector3 lastInteractDir;
+
+    private void Start()
+    {
+        gameInput.OnInteractHandler += GameInput_OnInteractHandler;
+    }
+
+    private void GameInput_OnInteractHandler(object sender, EventArgs e)
+    {
+        Vector2 inputVector = gameInput.GetPlayerMoveInput();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, counterLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent<ClearCounter>(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = input.GetPlayerMoveInput();
+        HandleMovement();
+        HandleInteractions();
+
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetPlayerMoveInput();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if (moveDir != Vector3.zero) {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, counterLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent<ClearCounter>(out ClearCounter clearCounter)) {
+                //clearCounter.Interact();
+            }
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetPlayerMoveInput();
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         isWalking = moveDir != Vector3.zero;
         float moveDistance = moveSpeed * Time.deltaTime;
@@ -29,10 +82,10 @@ public class Player : MonoBehaviour
         }
 
         float rotateSpeed = 10f;
-        if (isWalking) {
+        if (isWalking)
+        {
             transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         }
-        
     }
 
     public bool IsWalking()
